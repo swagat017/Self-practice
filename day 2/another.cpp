5d6a2842ff50986396947c07ca9deb440363d98c
swagat017 <134423648+swagat017@users.noreply.github.com>
2024-05-30 22:26:05 +0545

    day 2 done

---
+++ day 2/another.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define f(x) x *x + 4 * x + 4
+
+using namespace std;
+
+int main()
+{
+    cout << f(2);
+
+    return 0;
+}
--- day 2/include.cpp
+++ day 2/include.cpp
@@ -1,9 +1,24 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-int main ()
+int add(int a, int b)
 {
-    int x;
-    cout<<x; 
+    return a + b;
+}
+
+double add(double a, double b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int int1 = 10, int2 = 20;
+    double double1 = 5.5, double2 = 2.2;
+    std::string str1 = "Swagat", str2 = "am!";
+
+    std::cout << "Sum of integers: " << add(int1, int2) << std::endl;
+
+    std::cout << "Sum of doubles: " << add(double1, double2) << std::endl;
+
     return 0;
 }
